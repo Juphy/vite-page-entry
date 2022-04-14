13d8af25e4d4a6df320e6d85d3624389cf5b0b48
gaojuphy <1571066810@qq.com>
2022-04-14 19:15:10 +0800

    123

---
+++ src/components/parent.vue
@@ -0,0 +1,15 @@
+<template></template>
+
+<script>
+import { defineComponent, provide } from "vue"
+export default defineComponent({
+    setup(){
+        const count = ref(0)
+        const color = ref('#000')
+        provide('count', count)
+        provide('color', color)
+    }
+})
+</script>
+
+<style></style>
