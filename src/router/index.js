0f5e27c6d5194a8ac7a304032031ba972e3d7675
gaojuphy <1571066810@qq.com>
2022-04-22 01:11:45 +0800

    routes update

--- src/router/index.js
+++ src/router/index.js
@@ -6,8 +6,8 @@ const routes = [
 ]
 
 export const router = createRouter({
-    // history: createWebHistory(),
-    history: createWebHashHistory(),
+    history: createWebHistory(),
+    // history: createWebHashHistory(),
     routes
 })
 
