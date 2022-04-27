d7d436053c7c69e27fb1a6e849b6ca5ac1d67737
gaojuphy <1571066810@qq.com>
2022-04-27 23:06:30 +0800

    webrtc-server

--- server/webrtc-server.js
+++ server/webrtc-server.js
@@ -1,7 +1,7 @@
 // https://gitee.com/sevenzyh/learn-webrtc/tree/master
 const Koa = require("koa")
 const koaSend = require("koa-send")
-const static = require("koa-static")
+const statics = require("koa-static")
 const socket = require("socket.io")
 
 const path = require("path")
@@ -10,12 +10,25 @@ const http = require("http")
 const port = 3000
 const app = new Koa()
 
-app.use(static(
+// 设置静态资源放置的地址，可以写多个，多个静态资源文件
+// static(root, option)接收两个参数
+// root: 为路径，表示文件所在的相对路径
+// option: {
+//     maxage: 浏览器默认的最大缓存时长  max - age, 单位为毫秒，默认值为0，也就是不启用缓存。
+//     hidden: 是否允许传输隐藏的文件，默认为false， 不传输。
+//     index: 默认的文件名，默认值为index.html。
+//     defer: 是否推迟响应，如果为true, koa - static 将会咋其他中间件执行完成之后在执行。
+//     gzip: 如果客户端支持gzip压缩且资源文件后缀为.gz， 则进行gzip压缩，默认为true, 也就是支持gzip压缩。
+//     setHeaders: 设置请求头函数，格式为： fn(res, path, stats)。
+//     extensions: 当资源匹配不到的时候。根据传入的数组参数依次进行匹配，返回匹配到的第一个资源。
+// }
+
+app.use(statics(
     path.join(__dirname, './dist')
 ))
 
-app.use(async(ctx, next) => {
-    if(!/\./.test(ctx.request.url)){
+app.use(async (ctx, next) => {
+    if (!/\./.test(ctx.request.url)) {
         await koaSend(
             ctx,
             'index.html',
@@ -25,7 +38,7 @@ app.use(async(ctx, next) => {
                 gzip: true
             }
         )
-    }else{
+    } else {
         await next()
     }
 })
@@ -45,13 +58,67 @@ const httpIo = socket(httpServer, options)
 const rooms = {}
 const socks = {}
 const httpConnectIoCallBack = (sock) => {
+    console.log(`sockId:${sock.id}连接成功!!!`);
+    sock.emit('connectionSuccess', sock.id);
+    // 用户断开连接
+    sock.on('userLeave', ({ userName, roomId, sockId } = user) => {
+        console.log(`userName:${userName}, roomId:${roomId}, sockId:${sockId} 断开了连接...`);
+        if (roomId && rooms[roomId] && rooms[roomId].length) {
+            rooms[roomId] = rooms[roomId].filter(item => item.sockId !== sockId);
+            httpIo.in(roomId).emit('userLeave', rooms[roomId]);
+            console.log(`userName:${userName}, roomId:${roomId}, sockId:${sockId} 离开了房间...`);
+        }
+    });
+    // 用户加入房间
+    sock.on('checkRoom', ({ userName, roomId, sockId }) => {
+        rooms[roomId] = rooms[roomId] || [];
+        sock.emit('checkRoomSuccess', rooms[roomId]);
+        if (rooms[roomId].length > 1) return false;
+        rooms[roomId].push({ userName, roomId, sockId });
+        sock.join(roomId, () => {
+            httpIo.in(roomId).emit('joinRoomSuccess', rooms[roomId]);
+            socks[sockId] = sock;
+            console.log(`userName:${userName}, roomId:${roomId}, sockId:${sockId} 成功加入房间!!!`);
+        });
+    });
 
+    // 发送视频
+    sock.on('toSendVideo', (user) => {
+        httpIo.in(user.roomId).emit('receiveVideo', user);
+    });
+    // 取消发送视频
+    sock.on('cancelSendVideo', (user) => {
+        httpIo.in(user.roomId).emit('cancelSendVideo', user);
+    });
+    // 接收视频邀请
+    sock.on('receiveVideo', (user) => {
+        httpIo.in(user.roomId).emit('receiveVideo', user);
+    });
+    // 拒绝接收视频
+    sock.on('rejectReceiveVideo', (user) => {
+        httpIo.in(user.roomId).emit('rejectReceiveVideo', user);
+    });
+    // 接听视频
+    sock.on('answerVideo', (user) => {
+        httpIo.in(user.roomId).emit('answerVideo', user);
+    });
+    // 挂断视频
+    sock.on('hangupVideo', (user) => {
+        httpIo.in(user.roomId).emit('hangupVideo', user);
+    });
+    // addIceCandidate
+    sock.on('addIceCandidate', (data) => {
+        const toUser = rooms[data.user.roomId].find(item => item.sockId !== data.user.sockId);
+        socks[toUser.sockId].emit('addIceCandidate', data.candidate);
+    });
+    sock.on('receiveOffer', (data) => {
+        const toUser = rooms[data.user.roomId].find(item => item.sockId !== data.user.sockId);
+        socks[toUser.sockId].emit('receiveOffer', data.offer);
+    });
+    sock.on('receiveAnsewer', (data) => {
+        const toUser = rooms[data.user.roomId].find(item => item.sockId !== data.user.sockId);
+        socks[toUser.sockId].emit('receiveAnsewer', data.answer);
+    });
 }
 
-socket(httpServer).on("connection", (sock) => {
-    // 用户离开房间
-    sock.on("userleave", () => {})
-
-    // 检查房间是否可加入
-    sock.on("checkRoom", () => {})
-})
+httpIo.on('connection', httpConnectIoCallBack);
